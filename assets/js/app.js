338e7326ec889c991e7d5f63a944fa7447a03054
Colin Rudd <cnrudd@gmail.com>
2019-09-13 06:15:49 -0400

    fixed formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,107 +1,107 @@
-    var todoList = {};  // the object that will hold todos in the script
-    var lsKey = 'todoList';  // the name of the localStorage key
+var todoList = {};  // the object that will hold todos in the script
+var lsKey = 'todoList';  // the name of the localStorage key
 
-    //  On Click event associated with the add-to-do function
-    $("#add-to-do").on("click", function (event) {
-      // prevent form from submitting
-      event.preventDefault();
+//  On Click event associated with the add-to-do function
+$("#add-to-do").on("click", function (event) {
+    // prevent form from submitting
+    event.preventDefault();
 
-      // Get the to-do "value" from the textbox and store it a variable
-      var todo = $("#to-do").val().trim();
-      
-      // if todo is empty/blank, do nothing and exit the function
-      if (todo === '') return;
+    // Get the to-do "value" from the textbox and store it a variable
+    var todo = $("#to-do").val().trim();
 
-      // generate a random, unique id for this todo
-      var existingIds = Object.keys(todoList);
-      var id = makeRandomUniqueId(existingIds);
+    // if todo is empty/blank, do nothing and exit the function
+    if (todo === '') return;
 
-      addToDom(todo, id);
-      addToLocalStorage(todo, id);
+    // generate a random, unique id for this todo
+    var existingIds = Object.keys(todoList);
+    var id = makeRandomUniqueId(existingIds);
 
-      // Clear the textbox when done
-      $("#to-do").val('')
+    addToDom(todo, id);
+    addToLocalStorage(todo, id);
 
-    });
+    // Clear the textbox when done
+    $("#to-do").val('')
 
-    //  When a user clicks a check box then delete the specific content
-    //  (NOTE: Pay attention to the unusual syntax here for the click event.
-    //  Because we are creating click events on "dynamic" content, we can't just use the usual "on" "click" syntax.)
-    $(document.body).on("click", ".checkbox", function () {
+});
 
-      // Get the id of the button/todo from its data attribute and hold in a variable called todoId.
-      var todoId = $(this).attr("data-todo");
-      removeFromDom(todoId);
-      removeFromLocalStorage(todoId);
-    });
+//  When a user clicks a check box then delete the specific content
+//  (NOTE: Pay attention to the unusual syntax here for the click event.
+//  Because we are creating click events on "dynamic" content, we can't just use the usual "on" "click" syntax.)
+$(document.body).on("click", ".checkbox", function () {
 
-    function addToDom(todo, id) {
+    // Get the id of the button/todo from its data attribute and hold in a variable called todoId.
+    var todoId = $(this).attr("data-todo");
+    removeFromDom(todoId);
+    removeFromLocalStorage(todoId);
+});
 
-      // Create a new variable that will hold a "<p>" tag.
-      var $p = $('<p>');
-      $p.attr('id', 'item-' + id);
-      // Then give it an ID in the following form:
-      // "item-434544234"  // some random number suffix
-      // Then set the to-do "value" as text to this <p> element.
-      $p.text(todo);
+function addToDom(todo, id) {
 
-      // Create a button with unique identifiers based on what number it is in the list. Again use jQuery to do this.
-      // Give your button a data attribute called data-to-do and a class called "checkbox".
-      // Lastly add the letter X inside.
-      var $newBut = $("<button>");
-      $newBut.text('x');
-      $newBut.attr("id", "btn-" + id);
-      // Give your button a data attribute called data-to-do and a class called "checkbox".
-      $newBut.attr("data-todo", id);
-      $newBut.attr("class", "checkbox");
+    // Create a new variable that will hold a "<p>" tag.
+    var $p = $('<p>');
+    $p.attr('id', 'item-' + id);
+    // Then give it an ID in the following form:
+    // "item-434544234"  // some random number suffix
+    // Then set the to-do "value" as text to this <p> element.
+    $p.text(todo);
 
-      // Append the button to the to do item
-      $p.prepend($newBut);
+    // Create a button with unique identifiers based on what number it is in the list. Again use jQuery to do this.
+    // Give your button a data attribute called data-to-do and a class called "checkbox".
+    // Lastly add the letter X inside.
+    var $newBut = $("<button>");
+    $newBut.text('x');
+    $newBut.attr("id", "btn-" + id);
+    // Give your button a data attribute called data-to-do and a class called "checkbox".
+    $newBut.attr("data-todo", id);
+    $newBut.attr("class", "checkbox");
 
-      // Add the button and toDoItem to the to-dos div
-      $('#to-dos').prepend($p);
-    }
+    // Append the button to the to do item
+    $p.prepend($newBut);
 
-    function removeFromDom(id) {
-      // Select and Empty the specific <p> element that previously held the to do item.
-      $('#item-' + id).remove();
-    }
+    // Add the button and toDoItem to the to-dos div
+    $('#to-dos').prepend($p);
+}
 
-    function addToLocalStorage(todo, id) {
-        todoList[id] = todo;
-        localStorage.setItem(lsKey, JSON.stringify(todoList));
-      }
-  
-      function removeFromLocalStorage(id) {
-          delete todoList[id];
-          localStorage.setItem(lsKey, JSON.stringify(todoList));
-      }
+function removeFromDom(id) {
+    // Select and Empty the specific <p> element that previously held the to do item.
+    $('#item-' + id).remove();
+}
 
-    // this function loadTodosFromStorage is called once (below) when the page loads
-    function loadTodosFromStorage() {
-      var todos = localStorage.getItem(lsKey);
-      var listObj = JSON.parse(todos);
-      // if the key has no value in localStorage, just exit the function
-      if (!listObj) return;
+function addToLocalStorage(todo, id) {
+    todoList[id] = todo;
+    localStorage.setItem(lsKey, JSON.stringify(todoList));
+}
 
-      todoList = listObj;
+function removeFromLocalStorage(id) {
+    delete todoList[id];
+    localStorage.setItem(lsKey, JSON.stringify(todoList));
+}
 
-      Object.keys(todoList)       // get all the keys/ids as an array
-      .forEach(function(id) {     // iterate through the ids to add their corresponding todos to the DOM
-        var todo = todoList[id];
-        addToDom(todo, id);
-      });
-    }
+// this function loadTodosFromStorage is called once (below) when the page loads
+function loadTodosFromStorage() {
+    var todos = localStorage.getItem(lsKey);
+    var listObj = JSON.parse(todos);
+    // if the key has no value in localStorage, just exit the function
+    if (!listObj) return;
 
-    function makeRandomUniqueId(ids) {
-        const makeId = function() {return Math.random().toString().replace('0.', '')};
-        let ret = makeId();
-        // while ret (the new id) is found to already exist
-        // call makeId to make a new id and check if it already exits
-        while (ids && ids.includes(ret)) { ret = makeId()}
+    todoList = listObj;
 
-        // once new id is confirmed to be unique, return it
-        return ret;
-    }
+    Object.keys(todoList)       // get all the keys/ids as an array
+        .forEach(function (id) {     // iterate through the ids to add their corresponding todos to the DOM
+            var todo = todoList[id];
+            addToDom(todo, id);
+        });
+}
 
-    loadTodosFromStorage();
+function makeRandomUniqueId(ids) {
+    const makeId = function () { return Math.random().toString().replace('0.', '') };
+    let ret = makeId();
+    // while ret (the new id) is found to already exist
+    // call makeId to make a new id and check if it already exits
+    while (ids && ids.includes(ret)) { ret = makeId() }
+
+    // once new id is confirmed to be unique, return it
+    return ret;
+}
+
+loadTodosFromStorage();
